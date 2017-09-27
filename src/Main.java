0f1561fa05c756de7a9528965772c10658664a4b
DESKTOP-TKGTE9E\Matthew <matthew.ionescu@ufl.edu>
2017-09-27 13:12:58 -0400

    Hello world program

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class PACKAGE_NAME here
+ *
+ * @author Matthew
+ * @version 9/27/2017
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
